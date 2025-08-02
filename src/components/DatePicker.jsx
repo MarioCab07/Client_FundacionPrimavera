1d9f54719b43ee80460854f85e613f249d79052b
MarioCab07 <00002022@uca.edu.sv>
2025-08-02 12:13:39 -0600

    feat: add general section

--- src/components/DatePicker.jsx
+++ src/components/DatePicker.jsx
@@ -1,34 +1,25 @@
-import * as React from 'react';
+import * as React from "react";
 
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
-
-
-const DatePickerValue = ({date,setDate}) => {
-    
-    
-    return (
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
-               
-                
-                <DatePicker
-                    label="Fecha"
-                    value={date} 
-                    onChange={(newValue) => {
-                        
-                        if (newValue) {
-                            setDate(newValue);
-                            
-                        }
-                    }}
-                    
-                />
-            </div>
-        </LocalizationProvider>
-    );
+const DatePickerValue = ({ date, setDate, label }) => {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <div style={{ display: "flex", flexDirection: "column", gap: "16px" }}>
+        <DatePicker
+          label={label}
+          value={date}
+          onChange={(newValue) => {
+            if (newValue) {
+              setDate(newValue);
+            }
+          }}
+        />
+      </div>
+    </LocalizationProvider>
+  );
 };
 
 export default DatePickerValue;
